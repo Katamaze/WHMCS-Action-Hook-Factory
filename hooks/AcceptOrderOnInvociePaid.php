38e92cb61e5154e960f7fbaeb16ad5cb69e3dec8
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-07-23 19:45:39 +0200

    Create AcceptOrderOnInvociePaid.php

---
+++ hooks/AcceptOrderOnInvociePaid.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Automatically Accept when invoice is Paid
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+use WHMCS\Database\Capsule;
+
+add_hook('InvoicePaid', 1, function($vars)
+{
+    $orderID = Capsule::table('tblorders')->where('invoiceid', '=', $vars['invoiceid'])->pluck('id')[0];
+    if (!$orderID): return; endif;
+
+    $adminUsername = ''; // Optional for WHMCS 7.2 and later
+    localAPI('AcceptOrder', array('orderid' => $orderID), $adminUsername);
+});
