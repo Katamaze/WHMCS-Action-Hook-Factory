4a28ebb6c0dff01631ebbeac6c400e6851f7f933
Davide Mantenuto <68689332+davmante@users.noreply.github.com>
2020-07-23 13:30:49 +0200

    Create AddButtonNextToModulesFunctions.php

---
+++ hooks/AddButtonNextToModulesFunctions.php
@@ -0,0 +1,21 @@
+<?
+
+/**
+ * Add button next to module's functions
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+add_hook('AdminAreaHeaderOutput', 1, function($vars)
+{
+    return <<<HTML
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $("#modcmdbtns").append('<button type="button" class="btn btn-danger"><i class="fa fa-shower" aria-hidden="true"></i> Bath Time</button>');
+    });
+    </script>
+HTML;
+});
