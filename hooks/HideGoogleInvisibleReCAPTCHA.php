bbdf901ee07539bc6718b4ec4690e33bc2b39a3c
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-07-24 19:11:07 +0200

    Create HideGoogleInvisibleReCAPTCHA.php

---
+++ hooks/HideGoogleInvisibleReCAPTCHA.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Hide Google Invisible reCAPTCHA
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+add_hook('ClientAreaHeadOutput', 1, function($vars)
+{
+    return <<<HTML
+<style>
+.grecaptcha-badge { opacity:0;}
+</style>
+HTML;
+});
