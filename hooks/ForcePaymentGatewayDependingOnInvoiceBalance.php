c9d3816960a91d2db2800f685da8d5088e12a963
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-07-24 15:36:41 +0200

    Create ForcePaymentGatewayDependingOnInvoiceBalance.php

---
+++ hooks/ForcePaymentGatewayDependingOnInvoiceBalance.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Force Payment Gateway depending on Invoice Balance
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+use WHMCS\Database\Capsule;
+
+add_hook('ClientAreaPage', 1, function($vars)
+{
+    $gateway = 'banktransfer'; // Force this payment gateway when invoice balance is >= $limit. Use System Name (eg. banktransfer, paypal)
+    $limit = '1000'; // Specifiy the limit in WHMCS Default Currency. The hook automatically handles currency conversion
+    $countries = array(); // Optionally define countries where you want to apply this hook. Use ISO 3166-1 alpha-2 country codes (eg. IT, FR, US)
+    $europe = false; // Set true to use the hook on EU-based customers. This option can be used together with $countries
+
+    if ($vars['filename'] == 'viewinvoice' AND $_GET['id'])
+    {
+        if ($countries AND !in_array($vars['clientsdetails']['countrycode'], $countries)): return; endif;
+        if ($europe AND !in_array($vars['clientsdetails']['countrycode'], array('AT', 'BE', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FK', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IM', 'IT', 'LV', 'LT', 'LU', 'MT', 'MC', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB'))): return; endif;
+
+        if ($gateway AND $limit)
+        {
+            $currencyKey = array_keys(array_column($vars['currencies'], 'id'), $vars['clientsdetails']['currency'])[0];
+
+            if (!$vars['currencies'][$currencyKey]['default'])
+            {
+                $currencyRate = Capsule::table('tblcurrencies')->where('id', '=', $vars['currencies'][$currencyKey]['id'])->pluck('rate')[0];
+                $balance = number_format($vars['balance']->toNumeric() / $currencyRate, 2, '.', '');
+            }
+            else
+            {
+                $balance = $vars['balance']->toNumeric();
+            }
+
+            if ($balance >= $limit)
+            {
+                if ($vars['paymentmodule'] == $gateway)
+                {
+                    return array('allowchangegateway' => false);
+                }
+                else
+                {
+                    Capsule::table('tblinvoices')->where('id', $_GET['id'])->update(['paymentmethod' => $gateway]);
+                    header('Location: viewinvoice.php?id=' . $_GET['id']);
+                    die();
+                }
+            }
+        }
+    }
+});
