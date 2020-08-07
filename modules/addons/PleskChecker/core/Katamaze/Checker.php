297f29e1b13ec0112b2bde82246b77dc2abb9755
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-08-07 14:39:07 +0200

    Update Checker.php

--- modules/addons/PleskChecker/core/Katamaze/Checker.php
+++ modules/addons/PleskChecker/core/Katamaze/Checker.php
@@ -46,7 +46,7 @@ class Checker
             else
             {
                 $output['servers'][$v->server]['accounts'] = $i++;
-                $temp[$v->server][$v->id] = array('id' => $v->id, 'userid' => $v->userid, 'username' => $v->username, 'external-id' => $externalID[$v->userid]);
+                $temp[$v->server][] = array('id' => $v->id, 'userid' => $v->userid, 'username' => $v->username, 'external-id' => $externalID[$v->userid]);
             }
         }
 
@@ -64,14 +64,12 @@ class Checker
 <customer>
 EOF;
 
-                foreach ($packages as $pid => $package)
+                foreach ($packages as $package)
                 {
-                    $login = ($package['external-id'] ? $package['external-id'] : $package['username']);
-
                     $request .= <<<EOF
     <get>
         <filter>
-            <login>{$login}</login>
+            <login>{$package['username']}</login>
         </filter>
         <dataset>
             <gen_info/>
@@ -87,18 +85,20 @@ EOF;
 
                 $response = $plesk->request($request);
                 $response = new SimpleXMLElement($response);
+                $response = json_decode(json_encode($response), true);
                 $i = 0;
 
-                foreach ($response->customer->get as $v)
+                foreach ($response['customer']['get'] as $k => $v)
                 {
-                    if ($v->result->errcode == '1013')
+                    if ($v['result']['errcode'] == '1013' OR (!$v['result']['data']['gen_info']['external-id'] AND $temp[$serverID][$k]['external-id']))
                     {
-                        $key = array_search($v->result->{'filter-id'}->__toString(), $externalID);
-                        $hostingList = Capsule::table('tblhosting')->where('userid', $key)->where('server', $serverID)->pluck('domain', 'id');
-                        $output['error']['externalID'][$serverID][$key] = array('userid' => $key, 'external_id' => $v->result->{'filter-id'}->__toString(), 'server' => $serverID, 'accounts' => $hostingList);
+                        $hostingList = Capsule::table('tblhosting')->where('userid', $temp[$serverID][$k]['userid'])->where('server', $serverID)->pluck('domain', 'id');
+                        $output['error']['externalID'][$serverID][$temp[$serverID][$k]['userid']] = array('userid' => $temp[$serverID][$k]['userid'], 'external_id' => $v['result']['filter-id'], 'server' => $serverID, 'accounts' => $hostingList);
                         $i++;
                     }
                 }
+
+                unset($plesk, $request, $response);
             }
 
             $output['externalIDCount'] = $i;
