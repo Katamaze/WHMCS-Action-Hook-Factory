a6b5ba9d5f8247ed6cb6d03850db0b1a924f01c1
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-08-10 17:59:23 +0200

    Update RenameAddonModuleLabel.php

--- hooks/RenameAddonModuleLabel.php
+++ hooks/RenameAddonModuleLabel.php
@@ -14,7 +14,9 @@ add_hook('AdminAreaHeaderOutput', 1, function($vars)
     return <<<HTML
     <script type="text/javascript">
     $(document).ready(function(){
-        $("a[id='Menu-Addons-CURRENT NAME']").text('NEW NAME');
+        $("a[id='Menu-Addons-Mercury']").text('CMS');
+        $("a[id='Menu-Addons-Commission Manager']").text('Affiliates');
+        $("a[id='Menu-Addons-Billing Extension']").text('Accounting');
     });
     </script>
 HTML;
