5e054c5349c591b325b95d60dcc4a55a512ecf33
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-08-11 20:29:29 +0200

    Create TicketFeedbackAutoClose.php

---
+++ hooks/TicketFeedbackAutoClose.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Bulk Auto Recalculate Client Domain & Products/Services
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ *
+ */
+ 
+add_hook('EmailPreSend', 1, function($vars)
+{
+    if ($vars['messagename'] == 'Support Ticket Auto Close Notification' AND $vars['relid'])
+    {
+        $adminUsername = '';
+        $results = localAPI('SendEmail', array('messagename' => 'Support Ticket Feedback Request', 'id' => $vars['relid']), $adminUsername);
+    }
+});
