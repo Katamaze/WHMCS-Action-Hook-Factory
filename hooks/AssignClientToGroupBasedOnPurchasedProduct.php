fd49b764bfa767e201e8216b310abee41d1b6a07
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-08-14 16:27:20 +0200

    Update AssignClientToGroupBasedOnPurchasedProduct.php

--- hooks/AssignClientToGroupBasedOnPurchasedProduct.php
+++ hooks/AssignClientToGroupBasedOnPurchasedProduct.php
@@ -13,12 +13,19 @@ use WHMCS\Database\Capsule;
 
 add_hook('AcceptOrder', 1, function($vars)
 {
-    $productID  = 12; // Replace with the ID of your Product/Service
-    $groupID    = 1; // Replace with the ID of your Client Group
-    $userID     = Capsule::table('tblorders')->leftJoin('tblhosting', 'tblorders.id', '=', 'tblhosting.orderid')->where([['tblorders.id', '=', $vars['orderid']], ['tblhosting.packageid', '=', $productID]])->first(['tblorders.userid']);
+    // Define group/product pairs. Instructions provided below
+    // https://github.com/Katamaze/WHMCS-Free-Action-Hooks/blob/master/README.md#client-to-group-based-on-purchased-productservice
+    $groups['1'] = array('1', '2', '3');
+    $groups['2'] = array('4');
 
-    if ($userID)
+    $userID = Capsule::table('tblorders')->where('id', $vars['orderid'])->pluck('userid')[0];
+    $orderedProducts = Capsule::table('tblhosting')->where('orderid', $vars['orderid'])->pluck('packageid');
+
+    foreach ($groups as $group => $packages)
     {
-        Capsule::table('tblclients')->where('id', $userID->userid)->update(['groupid' => $groupID]);
+        if (array_intersect($orderedProducts, $packages))
+        {
+            Capsule::table('tblclients')->where('id', $userID)->where('groupid', '0')->update(['groupid' => $group]);
+        }
     }
 });
