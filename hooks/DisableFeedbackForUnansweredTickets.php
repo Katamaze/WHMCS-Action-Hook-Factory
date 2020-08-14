fc039b259ad9c1d6aa562bb865efc7fc6a287b17
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-08-14 15:26:42 +0200

    Create DisableFeedbackForUnansweredTickets.php

---
+++ hooks/DisableFeedbackForUnansweredTickets.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Disable Feedback Request for Unanswered Tickets
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+use WHMCS\Database\Capsule;
+
+add_hook('EmailPreSend', 1, function($vars)
+{
+    if ($vars['messagename'] == 'Support Ticket Feedback Request')
+    {
+        if (!Capsule::table('tblticketreplies')->where('tid', $vars['relid'])->where('admin', '!=', '')->pluck('id')[0])
+        {
+            return array('abortsend' => true);
+        }
+    }
+});
