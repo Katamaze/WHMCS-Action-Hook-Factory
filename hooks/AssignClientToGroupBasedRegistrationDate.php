5acc02fcea55b49227667d0463721a64adc88569
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-09-09 23:51:05 +0200

    Create AssignClientToGroupBasedRegistrationDate.php

---
+++ hooks/AssignClientToGroupBasedRegistrationDate.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Assign Client to Group based on registration date
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+use WHMCS\Database\Capsule;
+
+add_hook('DailyCronJob', 1, function($vars)
+{
+    // Define group/product pairs. Instructions provided below
+    // https://github.com/Katamaze/WHMCS-Free-Action-Hooks/blob/master/README.md#client-to-group-based-on-purchased-productservice
+    $groups['1'] = '90';
+    $groups['2'] = '180';
+    $groups['3'] = '365';
+
+    if (!$groups): return; endif;
+
+    foreach ($groups as $groupID => $days)
+    {
+        Capsule::table('tblclients')->whereRaw('DATEDIFF(CURDATE(), datecreated) >= "' . $days . '"')->update(['groupid' => $groupID]);
+    }
+});
