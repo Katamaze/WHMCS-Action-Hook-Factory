45dc60ab53e1ff31f02906aa87e42b71522c0eac
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-11-19 15:09:29 +0100

    Update AcceptOrderOnInvociePaid.php

--- hooks/AcceptOrderOnInvociePaid.php
+++ hooks/AcceptOrderOnInvociePaid.php
@@ -8,15 +8,24 @@
  * @link        https://katamaze.com
  * @author      Davide Mantenuto <info@katamaze.com>
  */
+
 use WHMCS\Database\Capsule;
 
 add_hook('InvoicePaid', 1, function($vars)
 {
-    $invoiceAmount = false; // Auto-accept order only if invoice amount is >= $invoiceAmount. Set false to auto-accept everything (Important: currency conversion not supported)
-
-    if ($invoiceAmount AND Capsule::table('tblinvoices')->where('id', '=', $vars['invoiceid'])->where('total', '>=', $invoiceAmount)->pluck('id')[0]): return; endif;
     $orderID = Capsule::table('tblorders')->where('invoiceid', '=', $vars['invoiceid'])->pluck('id')[0];
     if (!$orderID): return; endif;
+
+    $invoiceTotal = 0.93; // Auto-accept order based on invoice total. Set false to auto-accept everything. The hook automatically handles currency conversion
+    $operator = '<='; // Use ">=" to auto-accept orders greater than or equal to $invoiceTotal. Use "<=" for less than or equal to $invoiceTotal
+
+    if ($invoiceTotal)
+    {
+        $currency = Capsule::select(Capsule::raw('SELECT t3.rate FROM tblinvoices AS t1 LEFT JOIN tblclients AS t2 ON t1.userid = t2.id LEFT JOIN tblcurrencies AS t3 ON t2.currency = t3.id WHERE t1.id = "' . $vars['invoiceid'] . '" AND t3.default = "0" LIMIT 1'))[0];
+        $invoiceTotal = ($currency ? $invoiceTotal * $currency->rate : $invoiceTotal);
+
+        if (Capsule::table('tblinvoices')->where('id', '=', $vars['invoiceid'])->where('total', ($operator == '>=' ? '<=' : '>='), $invoiceTotal)->pluck('id')[0]): echo 'NON FACCIO L\'AUTO ACCEPT'; return; endif;
+    }
 
     $adminUsername = ''; // Optional for WHMCS 7.2 and later
     localAPI('AcceptOrder', array('orderid' => $orderID), $adminUsername);
