a942657f4c05f38e0c920a475744494332f5b7b8
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-11-28 15:54:32 +0100

    Update RestrictDomainBillingCyclesBasedOnTLD.php

--- hooks/RestrictDomainBillingCyclesBasedOnTLD.php
+++ hooks/RestrictDomainBillingCyclesBasedOnTLD.php
@@ -15,23 +15,40 @@ use WHMCS\Database\Capsule;
 
 add_hook('ClientAreaPage', 1, function($vars)
 {
-    $restrictedTLDs = array('.com'); // Specify TLD for which you want to restrict billing cycles. TLD must start with a dot "."
-    $restrictedPeriods = array('2', '3', '4', '5', '6', '7', '8', '9', '10'); // Any value from 1 to 10 (1 year, 2 years, 3 years... 10 years)
-
-    if (!$restrictedTLDs OR !$restrictedPeriods): return; endif;
-
-    foreach ($vars['domains'] as $k => $v)
+    if (($vars['filename'] == 'cart' AND $_GET['a'] == 'view') OR $vars['templatefile'] == 'domain-renewals')
     {
-        $tld = Capsule::select(Capsule::raw('SELECT extension FROM tbldomainpricing WHERE "' . $v['domain'] . '" LIKE CONCAT("%", extension) ORDER BY LENGTH(extension) DESC LIMIT 1'))[0]->extension;
+        $restrictedTLDs = array('.com'); // Specify TLD for which you want to restrict billing cycles. TLD must start with a dot "."
+        $restrictedPeriods = array('2', '3', '4', '5', '6', '7', '8', '9', '10'); // Any value from 1 to 10 (1 year, 2 years, 3 years... 10 years)
 
-        if (in_array($tld, $restrictedTLDs))
+        if (!$restrictedTLDs OR !$restrictedPeriods): return; endif;
+        $key = ($vars['templatefile'] == 'domain-renewals' ? 'renewalsData' : 'domains');
+
+        foreach ($vars[$key] as $k => $v)
         {
-            foreach ($restrictedPeriods as $i)
+            $tld = Capsule::select(Capsule::raw('SELECT extension FROM tbldomainpricing WHERE "' . $v['domain'] . '" LIKE CONCAT("%", extension) ORDER BY LENGTH(extension) DESC LIMIT 1'))[0]->extension;
+
+            if (in_array($tld, $restrictedTLDs))
             {
-                unset($vars['domains'][$k]['pricing'][$i]);
+                if ($key == 'domains')
+                {
+                    foreach ($restrictedPeriods as $i)
+                    {
+                        unset($vars[$key][$k]['pricing'][$i]);
+                    }
+                }
+                else
+                {
+                    foreach ($vars[$key][$k]['renewalOptions'] as $k2 => $v2)
+                    {
+                        if (in_array($vars[$key][$k]['renewalOptions'][$k2]['period'], $restrictedPeriods))
+                        {
+                            unset($vars[$key][$k]['renewalOptions'][$k2]);
+                        }
+                    }
+                }
             }
         }
-    }
 
-    return array('domains' => $vars['domains']);
+        return array($key => $vars[$key]);
+    }
 });
