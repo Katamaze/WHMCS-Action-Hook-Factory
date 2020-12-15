55bce8f1558f325784aaec4b29a3678fc7b9e929
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-12-15 03:54:08 +0100

    Update StrongerPasswordGeneratorForAutoProvisioning_v1.php

--- hooks/StrongerPasswordGeneratorForAutoProvisioning_v1.php
+++ hooks/StrongerPasswordGeneratorForAutoProvisioning_v1.php
@@ -11,7 +11,7 @@
 
 use WHMCS\Database\Capsule;
 
-add_hook('PreModuleCreate', 1, function($vars)
+add_hook('OverrideModuleUsernameGeneration', 1, function($vars)
 {
     $password = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=+?'), 0, $length = '10');
     Capsule::table('tblhosting')->where('id', $vars['params']['serviceid'])->update(['password' => Encrypt($password)]);
