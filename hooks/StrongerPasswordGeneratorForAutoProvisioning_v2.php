e7addd816ddcd78a5b3bfecbe888800526a9275b
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2020-12-15 03:54:12 +0100

    Update StrongerPasswordGeneratorForAutoProvisioning_v2.php

--- hooks/StrongerPasswordGeneratorForAutoProvisioning_v2.php
+++ hooks/StrongerPasswordGeneratorForAutoProvisioning_v2.php
@@ -11,7 +11,7 @@
 
 use WHMCS\Database\Capsule;
 
-add_hook('PreModuleCreate', 1, function($vars)
+add_hook('OverrideModuleUsernameGeneration', 1, function($vars)
 {
     $chars              = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     $specialChars       = '!@#$%^&*?'; // Plesk does not consider (, ), -, = and + as special characters
