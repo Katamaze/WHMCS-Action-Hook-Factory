33e91530cc94343a1b21bc0448616bd8993dd436
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2021-03-22 12:49:12 +0100

    Update ForcePaymentGatewayDependingOnInvoiceBalance.php

--- hooks/ForcePaymentGatewayDependingOnInvoiceBalance.php
+++ hooks/ForcePaymentGatewayDependingOnInvoiceBalance.php
@@ -14,7 +14,7 @@ use WHMCS\Database\Capsule;
 define('kt_gateway', 'banktransfer'); // Force this payment gateway when invoice balance is >= $limit. Use System Name (eg. banktransfer, paypal)
 define('kt_limit', '0'); // Specifiy the limit in WHMCS Default Currency. The hook automatically handles currency conversion (0 to disable)
 define('kt_countries', array()); // Optionally define countries where you want to apply this hook. Use ISO 3166-1 alpha-2 country codes (eg. IT, FR, US)
-define('kt_europe', true); // Set true to use the hook on EU-based customers. This option can be used together with $countries
+define('kt_europe', false); // Set true to use the hook on EU-based customers. This option can be used together with $countries
 
 add_hook('ClientAreaPage', 100, function($vars) {
 
