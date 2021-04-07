4beb29891fe843f6cba822edd69a00b16c8ada8d
Kian987 <kian987@gmail.com>
2021-04-07 14:21:38 +0200

    Create InactiveIsTheNewActive.php

---
+++ hooks/InactiveIsTheNewActive.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Inactive is the new Active? Just testing stuff
+ *
+ * @package     WHMCS
+ * @copyright   Katamaze
+ * @link        https://katamaze.com
+ * @author      Davide Mantenuto <info@katamaze.com>
+ */
+
+/*
+ * I decided to remove the script as I don't know if it breaches WHMCS license
+ */
