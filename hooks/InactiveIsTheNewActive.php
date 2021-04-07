64f7e2b444686a5a8fbc289aba1ca158ac22163f
Davide Mantenuto <68689332+Kian987@users.noreply.github.com>
2021-04-07 14:08:17 +0200

    Update InactiveIsTheNewActive.php

--- hooks/InactiveIsTheNewActive.php
+++ hooks/InactiveIsTheNewActive.php
@@ -9,33 +9,6 @@
  * @author      Davide Mantenuto <info@katamaze.com>
  */
 
-use WHMCS\Database\Capsule;
-
-add_hook('AdminAreaHeadOutput', 1, function($vars) {
-
-    if ($vars['filename'] == 'clients' OR in_array($_GET['rp'], array('/admin/services', '/admin/addons', '/admin/domains'))) {
-
-        $autoPost = <<<HTML
-if ($('input#checkboxShowHidden').is(':checked')) {
-
-    $('body').addClass('hidden');
-    $('#checkboxShowHidden').click();
-}
-HTML;
-
-    }
-
-    return <<<HTML
-<script>
-$(document).on('ready', function() {
-
-    if ($('input#intelliSearchHideInactiveSwitch').is(':checked')) {
-
-        $('#intelliSearchHideInactiveSwitch').click();
-    }
-
-    {$autoPost}
-})
-</script>
-HTML;
-});
+/*
+ * I decided to remove the script as I don't know if it breaches WHMCS license
+ */
